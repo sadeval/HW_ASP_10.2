2b444ac9493b843c5add26f1f2d507de142a35b2
sadeval <sadeval2011@gmail.com>
2024-12-11 16:26:34 +0200

    Add project files.

---
+++ IWeatherService.cs
@@ -0,0 +1,35 @@
+﻿namespace HW_ASP_10._2
+{
+    public interface IWeatherService
+    {
+        Task<WeatherInfo> GetWeatherAsync(string city);
+    }
+
+    public class WeatherInfo
+    {
+        public string City { get; set; } = string.Empty;
+        public string Condition { get; set; } = "Солнечно";
+        public double Temperature { get; set; }
+    }
+
+    public class FakeWeatherService : IWeatherService
+    {
+        private static Random rnd = new Random();
+        private static string[] conditions = new[] { "Солнечно", "Дождь", "Облачно", "Пасмурно", "Снег" };
+
+        public Task<WeatherInfo> GetWeatherAsync(string city)
+        {
+            // Случайно выбираем состояние из массива conditions
+            var randomCondition = conditions[rnd.Next(conditions.Length)];
+
+            // Возвращаем фейковые данные
+            var info = new WeatherInfo
+            {
+                City = city,
+                Condition = randomCondition,
+                Temperature = 15 + rnd.NextDouble() * 10 // от 15 до 25 градусов
+            };
+            return Task.FromResult(info);
+        }
+    }
+}
---
+++ ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HW_ASP_10._2.ViewComponents
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        private readonly IWeatherService _weatherService;
+
+        public WeatherViewComponent(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string city)
+        {
+            var weather = await _weatherService.GetWeatherAsync(city);
+            return View(weather);
+        }
+    }
+}
